d6a1c959f8bd491481ba2aabde572fcc6576503a
girishlc <girish.lc@gmail.com>
2025-01-04 15:44:21 +0530

    second project completion

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Project
+from .models import Blog
 
-admin.site.register(Project)
+admin.site.register(Blog)
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Project(models.Model):
-    title = models.CharField(max_length=100)
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
     datefld = models.DateField()
-    description = models.CharField(max_length=250)
+    description = models.TextField()
     
+    def __str__(self):
+        return self.title
